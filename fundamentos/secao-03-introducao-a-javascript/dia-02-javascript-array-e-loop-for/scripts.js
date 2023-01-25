5c7164f3047d3319aed6e00e8d1d160a6950e46e
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-01-25 11:25:01 -0300

    concluido exercicios menos o 5 e 8

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,87 @@
+            // parte-1-Percorra o array imprimindo todos os valores nele contidos com a função console.log
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+            // parte-2-Some todos os valores contidos no array e imprima o resultado;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index] + numbers[index]);
+// }
+
+
+            // parte -3-Calcule e imprima a média aritmética dos valores contidos no array;
+            // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     resultado = resultado + numbers[index];
+// }
+// console.log(resultado);
+
+            // parte -4-Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+// let media = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     resultado = resultado + numbers[index];
+//   media = resultado / numbers.length;
+// } 
+// console.log(media);
+
+              // parte 5-Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+// let media = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     resultado = resultado + numbers[index];
+//     media = resultado / numbers.length;          
+// }
+//   if (media < 20) {
+//     console.log("Valor menor que 20");
+//   } else {
+//     console.log("Valor maior que 20");
+//   }
+
+            // parte - 6-Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+
+
+
+
+            // parte - 7-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let impar= 0;
+//   for (let index = 0; index < numbers.length; index += 1){
+//     if (numbers[index] % 2 !== 0) {
+//       impar = impar + 1;
+//      }
+//   }
+//   if (impar > 0) {
+//     console.log(impar);
+//   } else {
+//     console.log("nenhum valor ímpar encontrado");
+//   }
+
+              // parte -8
+  
+
+              // parte -9-Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+// let array = [];
+// for (let index = 1; index <= 25; index += 1) {
+//   array.push(index)
+// }
+// console.log(array);
+
+            // parte - 10-Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+  // let array = [];
+  // for (let index = 1; index <= 25; index += 1) {
+  //   console.log(index / 2);
+  // }
+  
